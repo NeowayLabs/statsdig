a12e90eb8325f8387923eca2e1d247e9ed295dee
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-11-22 12:38:46 -0200

    Revert receiver for a simpler naive solution

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -13,42 +13,19 @@ func panicAtTheDisco(err error) {
 	}
 }
 
-type metric struct {
-	addr net.Addr
-	msg  string
-}
-
-func listener(conn net.PacketConn, received chan<- metric) {
-
-	packet := make([]byte, 1024)
-	for {
-		_, addr, err := conn.ReadFrom(packet)
-		panicAtTheDisco(err)
-		received <- metric{addr: addr, msg: string(packet)}
-	}
-}
-
 func main() {
 	var port int
 	flag.IntVar(&port, "port", 8125, "port to listen to")
 
 	counter := 0
-	received := make(chan metric, 10000)
-
 	conn, err := net.ListenPacket("udp", fmt.Sprintf(":%d", port))
 	panicAtTheDisco(err)
 
-	listeners := 100
-
-	for i := 0; i < listeners; i++ {
-		go listener(conn, received)
-	}
-
+	packet := make([]byte, 1024)
 	for {
-		log.Printf("Listening for packages at: %d", port)
-		m := <-received
+		_, _, err := conn.ReadFrom(packet)
+		panicAtTheDisco(err)
 		counter += 1
-		log.Printf("Read: %s from: %s", m.msg, m.addr)
-		log.Printf("Total received: %d", counter)
+		log.Println(string(packet), counter)
 	}
 }
