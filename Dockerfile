4b2a830d121ed4b7bcbf5fb87a1d3d0231f08465
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-11-22 17:36:34 -0200

    Add code for preferred client

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY ./ /go/src/github.com/NeowayLabs/statsdig
 
 WORKDIR /go/src/github.com/NeowayLabs/statsdig
 
+RUN go get ./...
 RUN go build ./cmd/sender
--- sampler.go
+++ sampler.go
@@ -5,23 +5,19 @@ package statsdig
 
 import (
 	"fmt"
-	"net"
+
+	"github.com/cactus/go-statsd-client/statsd"
 )
 
 // Sampler is how you will be able to sample metrics.
 type Sampler struct {
-	conn net.PacketConn
-	addr *net.UDPAddr
-}
-
-func (s *Sampler) write(data []byte) (int, error) {
-	return s.conn.WriteTo(data, s.addr)
+	client statsd.Statter
 }
 
 // NewSysdigSampler creates a sampler suited to work
 // with the sysdig cloud client, sending metrics to localhost
 // at the default statsd port.
-func NewSysdigSampler(addr string) (*Sampler, error) {
+func NewSysdigSampler() (*Sampler, error) {
 	return NewSampler("127.0.0.1:8125")
 }
 
@@ -30,44 +26,17 @@ func NewSysdigSampler(addr string) (*Sampler, error) {
 // where addr must be formatted just as the addr provided
 // to Go's net.ResolveUDPAddr function.
 func NewSampler(addr string) (*Sampler, error) {
-	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	client, err := statsd.NewClient(addr, "test-client")
 	if err != nil {
 		return nil, fmt.Errorf("resolve udp address failed: %s", err)
 	}
-	conn, err := net.ListenPacket("udp", ":0")
-	if err != nil {
-		return nil, fmt.Errorf("connection creation failed: %s", err)
-	}
 	return &Sampler{
-		conn: conn,
-		addr: udpAddr,
+		client: client,
 	}, nil
 }
 
 // Count sends a counter metric as specified here:
 // https://github.com/b/statsd_spec#counters
 func (sampler *Sampler) Count(name string) error {
-	countType := "c"
-	message := format(name, 1, countType)
-	n, err := sampler.write(message)
-	if err != nil {
-		return err
-	}
-	if n != len(message) {
-		return fmt.Errorf(
-			"expected to send %d but sent %d",
-			len(message),
-			n,
-		)
-	}
-	return nil
-}
-
-func format(name string, value int, metricType string) []byte {
-	return []byte(fmt.Sprintf(
-		"%s:%d|%s",
-		name,
-		value,
-		metricType,
-	))
+	return sampler.client.Inc(name, 1, 1.0)
 }
