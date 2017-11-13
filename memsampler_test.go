97b81a81fe238e5abc4ed79891bfcf8fb5dbcb44
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2017-11-13 11:31:23 -0200

    test time metric

--- memsampler_test.go
+++ memsampler_test.go
@@ -3,6 +3,7 @@ package statsdig_test
 import (
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/NeowayLabs/statsdig"
 )
@@ -19,18 +20,21 @@ func TestMemSampler(t *testing.T) {
 	}
 
 	wg.Add(count)
+	expectedTime := time.Duration(1000 * time.Millisecond)
 	for i := 0; i < count; i++ {
 		go func() {
 			s.Count("count")
 			s.Count("count", tags...)
 			s.Gauge("gauge", 666)
 			s.Gauge("gauge", 777, tags...)
+			s.Time("time", expectedTime)
+			s.Time("time", expectedTime, tags...)
 			wg.Done()
 		}()
 	}
 	wg.Wait()
 
-	checkCount := func(name string, counter func() int) {
+	checkMetric := func(name string, counter func() int) {
 		if counter() != count {
 			t.Fatalf(
 				"%s: expected %d but got %d",
@@ -41,19 +45,27 @@ func TestMemSampler(t *testing.T) {
 		}
 	}
 
-	checkCount("count", func() int {
+	checkMetric("count", func() int {
 		return s.GetCount("count")
 	})
 
-	checkCount("countWithTags", func() int {
+	checkMetric("countWithTags", func() int {
 		return s.GetCount("count", tags...)
 	})
 
-	checkCount("gauge", func() int {
+	checkMetric("gauge", func() int {
 		return s.GetGauge("gauge", 666)
 	})
 
-	checkCount("gaugeWithTags", func() int {
+	checkMetric("gaugeWithTags", func() int {
 		return s.GetGauge("gauge", 777, tags...)
+	})
+
+	checkMetric("time", func() int {
+		return s.GetTime("time", expectedTime)
+	})
+
+	checkMetric("timeWithTags", func() int {
+		return s.GetTime("time", expectedTime, tags...)
 	})
 }
